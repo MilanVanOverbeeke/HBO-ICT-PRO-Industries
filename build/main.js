bae81a64a8752ad847fb9c1703840009f0357821
MilanVanOverbeeke <90184115+MilanVanOverbeeke@users.noreply.github.com>
2022-01-23 21:38:42 +0100

    Delete build directory

--- build/main.js
+++
@@ -1,8 +0,0 @@
-import Game from './Game.js';
-console.log('Javascript is working!');
-window.addEventListener('load', () => {
-    console.log('Handling the Load event');
-    const game = new Game(document.getElementById('canvas'));
-    game.start();
-});
-//# sourceMappingURL=main.js.map
